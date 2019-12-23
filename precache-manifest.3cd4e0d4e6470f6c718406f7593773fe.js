e3b0672d2fab60f38b94541f940fc01a5305f078
Diego Chagas <drochachagas@gmail.com>
2019-12-23 09:39:42 -0300

    Updates

---
+++ precache-manifest.3cd4e0d4e6470f6c718406f7593773fe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ac975ac7dd288e4b360c571caaabf7c",
+    "url": "/WhenIWillLoseMyWeight/index.html"
+  },
+  {
+    "revision": "94ddaeceed9c4d5c3f1a",
+    "url": "/WhenIWillLoseMyWeight/static/js/2.3e61638c.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/WhenIWillLoseMyWeight/static/js/2.3e61638c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "320722f3647ec21db607",
+    "url": "/WhenIWillLoseMyWeight/static/js/main.6a16674e.chunk.js"
+  },
+  {
+    "revision": "5bac108d34407b84072c",
+    "url": "/WhenIWillLoseMyWeight/static/js/runtime-main.bb92c826.js"
+  }
+]);
